aa2c000bf33579fbdbc58fc647d161155aea52c4
SurprisedRobin <Robin99@hotmail.dk>
2023-03-14 00:50:30 +0100

    Addid Register and LogIn FINALLY!!!

---
+++ SurfUPWeb/Areas/Identity/Data/ApplicationUsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SurfUPWeb.Areas.Identity.Data
+{
+    public class ApplicationUsers : IdentityUser
+    {  
+        
+    }
+}
--- SurfUPWeb/Program.cs
+++ SurfUPWeb/Program.cs
@@ -1,7 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using SurfUPWeb.Data;
+using Microsoft.AspNetCore.Identity;
+using SurfUPWeb.Areas.Identity.Data;
+using Microsoft.VisualBasic;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("UserDbContextConnection");
+
+builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUsers>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<UserDbContext>();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,6 +20,14 @@ builder.Services.AddDbContext<MVCSurfUpDB>(options =>
 options.UseSqlServer(builder.Configuration
 .GetConnectionString("MVCSurfUpDBConnectionString")));
 
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<UserDbContext>(options =>
+options.UseSqlServer(builder.Configuration
+.GetConnectionString("UserDbContextConnection")));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -24,11 +43,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();;
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
